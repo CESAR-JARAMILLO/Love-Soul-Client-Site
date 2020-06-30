45ba2a865d8e8d763ca82e5a41ebf5de136b7ebb
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2020-06-30 14:29:26 -0700

    added automatic emails functionality

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_mail import Mail, Message
+
 
 app = Flask(__name__)
+mail= Mail(app)
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'testemailblah123@gmail.com'
+app.config['MAIL_PASSWORD'] = 'Testemail123!'
+app.config['MAIL_DEFAULT_SENDER'] ='testemailblah123@gmail.com'
+app.config['MAIL_USE_SSL'] = True
+mail= Mail(app)
 
 @app.route('/')
 def index():
@@ -9,6 +20,19 @@ def index():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.route('/result', methods=['POST', 'GET'])
+def result():
+    if request.method == 'POST':
+        name = request.form.get("name")
+        email = request.form.get("email")
+        message = request.form.get("message")
+        msg = Message(request.form.get("name"), recipients=['csarjaramillo89@yahoo.com'])
+        msg.body = 'Name: ' + name + '\n Email: ' + email + '\n Message: ' + message
+        mail.send(msg)
+        return render_template('result.html', result="Success!")
+    else:
+        return render_template('result.html', result="Failure!")
 
 @app.route('/about')
 def about():
